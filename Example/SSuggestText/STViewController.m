3cec9419750c463b14448b005b86277d96db68a7
saulogt <saulogt@gmail.com>
2014-08-17 23:56:37 -0300

    new pod format

---
+++ Example/SSuggestText/STViewController.m
@@ -0,0 +1,29 @@
+//
+//  STViewController.m
+//  SSuggestText
+//
+//  Created by saulogt on 08/17/2014.
+//  Copyright (c) 2014 saulogt. All rights reserved.
+//
+
+#import "STViewController.h"
+
+@interface STViewController ()
+
+@end
+
+@implementation STViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Example/SSuggestText/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SSuggestText
+//
+//  Created by saulogt on 08/17/2014.
+//  Copyright (c) 2014 saulogt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "STAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([STAppDelegate class]));
+    }
+}
